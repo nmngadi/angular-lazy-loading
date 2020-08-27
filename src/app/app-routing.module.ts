87d8adf407c5321fef0be304737006e2bcccf9b6
NMngadi <NMngadi@DVTL35KH3X2>
2020-08-27 11:03:18 +0200

    Add route paths to various components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
